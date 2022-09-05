2950e47ec8b29f7fd7365342912e2191d88a16da
Imranshaikh268 <111740994+Imranshaikh268@users.noreply.github.com>
2022-09-05 13:50:14 +0530

    Add files via upload

---
+++ variable_study.java
@@ -0,0 +1,40 @@
+package basic;
+
+public class variable_study {
+
+	public static void main(String[] args) {
+		int marks;//variable declaration
+		marks=59;//variable initinalization
+		System.out.println("my mark is "+marks);//usage
+/* first line commeent---.
+ *next comment line
+ * multiple coomment line
+ */
+		byte rollno;//variable declaration
+		rollno=5;// variable initinaltization
+		short idno;
+		idno=456;
+		long contactno;
+		contactno=1234567890;
+		float weight;
+	    weight =56.8f;
+	    double goldweight;
+	    goldweight=56.234566666655777777d;
+	    char grade;
+	    grade='h';
+	    boolean bulbswitch;
+	    bulbswitch=true;
+	    String StudentName;
+	    StudentName="Aisha";
+		System.out.println("my rollno is "+rollno);//usage
+		System.out.println("my idno is "+idno);
+		System.out.println("my mobileno is "+contactno);
+		System.out.println("my float is "+weight);
+		System.out.println("my goldweight is "+goldweight);
+		System.out.println("my grade is "+grade);
+		System.out.println("bulbswitch is"+bulbswitch);
+		System.out.println("My StudentName is "+StudentName);
+		
+	}
+
+}
